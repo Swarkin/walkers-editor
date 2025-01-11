9e4bb05c1d5d42dad1a6ff9b2ae714d623153659
swarkin <102416174+Swarkin@users.noreply.github.com>
2025-01-11 19:22:23 +0100

    Increase default window size

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod app;
 
 use app::MyApp;
+use eframe::egui::ViewportBuilder;
 
 fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default().with_inner_size([1000.0, 800.0]),
+        ..Default::default()
+    };
+    
     eframe::run_native(
         "MyApp",
-        Default::default(),
+        options,
         Box::new(|cc| Ok(Box::new(MyApp::new(cc.egui_ctx.clone())))),
     )
 }
