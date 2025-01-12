a3a42d012807bde9661c7393fe31e1d5b610014f
swarkin <102416174+Swarkin@users.noreply.github.com>
2025-01-12 04:48:11 +0100

    Code refactor and initial sidewalk overlay creation

--- src/app/editor/consts.rs
+++ src/app/editor/consts.rs
@@ -2,8 +2,9 @@ pub mod osm;
 
 use eframe::egui::Color32;
 
-pub const INTERACTION_RANGE: f32 = 4.0;
-pub const SELECTION_COLOR: Color32 = Color32::from_rgb(100, 200, 255);
+pub const HOVER_COLOR: Color32 = Color32::from_rgb(100, 200, 255);
+pub const HOVER_SIZE_INCREASE: f32 = 1.0;
+pub const SELECTION_COLOR: Color32 = Color32::from_rgb(40, 180, 255);
 pub const SELECTION_SIZE_INCREASE: f32 = 2.0;
 pub const DEFAULT_COLOR: Color32 = Color32::GRAY;
 
--- src/app/editor/visual.rs
+++ src/app/editor/visual.rs
@@ -3,7 +3,7 @@ use super::consts::osm::*;
 use super::consts::*;
 use eframe::egui;
 use eframe::epaint::PathStroke;
-use egui::{Color32, Pos2, Shape, Vec2};
+use egui::{Color32, Pos2, Shape, Window};
 use osm_parser::Way;
 
 #[derive(Debug, Default, Clone, Copy)]
@@ -20,44 +20,38 @@ pub const HIGHWAYS_WITH_SIDEWALK: &[&str; 15] = &[
 ];
 
 pub fn determine_width_default(w: &Way) -> f32 {
-	if let Some(tags) = &w.tags {
-		if let Some(building) = tags.get("building") {
-			return match building.as_str() {
-				"no" => DEFAULT_WIDTH,
-				_ => BUILDING_WIDTH,
-			}
-		} else if let Some(highway) = tags.get("highway") {
-			return match highway.as_str() {
-				"path" | "footway" | "steps" => PATH_WIDTH,
-				"service" | "track" => SERVICE_ROAD_WIDTH,
-				"residential" => MINOR_ROAD_WIDTH,
-				"tertiary" | "secondary" | "primary" | "trunk" | "motorway" |
-				"tertiary_link" | "secondary_link" | "primary_link" | "trunk_link" | "motorway_link" => MAJOR_ROAD_WIDTH,
-				_ => DEFAULT_WIDTH,
-			}
+	if let Some(building) = w.tags.get("building") {
+		return match building.as_str() {
+			"no" => DEFAULT_WIDTH,
+			_ => BUILDING_WIDTH,
 		}
-	}
-	DEFAULT_WIDTH
+	} else if let Some(highway) = w.tags.get("highway") {
+		return match highway.as_str() {
+			"path" | "footway" | "steps" => PATH_WIDTH,
+			"service" | "track" => SERVICE_ROAD_WIDTH,
+			"residential" => MINOR_ROAD_WIDTH,
+			"tertiary" | "secondary" | "primary" | "trunk" | "motorway" |
+			"tertiary_link" | "secondary_link" | "primary_link" | "trunk_link" | "motorway_link" => MAJOR_ROAD_WIDTH,
+			_ => DEFAULT_WIDTH,
+		}
+	} else { DEFAULT_WIDTH }
 }
 
 pub fn determine_color_default(w: &Way) -> Color32 {
-	if let Some(tags) = &w.tags {
-		if let Some(building) = tags.get("building") {
-			return match building.as_str() {
-				"no" => DEFAULT_COLOR,
-				_ => BUILDING_COLOR,
-			}
-		} else if let Some(highway) = tags.get("highway") {
-			return match highway.as_str() {
-				"path" => PATH_COLOR,
-				"footway" => FOOTWAY_COLOR,
-				"steps" => STEPS_COLOR,
-				"track" => TRACK_COLOR,
-				_ => Color32::WHITE,
-			}
+	if let Some(building) = w.tags.get("building") {
+		return match building.as_str() {
+			"no" => DEFAULT_COLOR,
+			_ => BUILDING_COLOR,
 		}
-	}
-	DEFAULT_COLOR
+	} else if let Some(highway) = w.tags.get("highway") {
+		return match highway.as_str() {
+			"path" => PATH_COLOR,
+			"footway" => FOOTWAY_COLOR,
+			"steps" => STEPS_COLOR,
+			"track" => TRACK_COLOR,
+			_ => Color32::WHITE,
+		}
+	} else { DEFAULT_COLOR }
 }
 
 
@@ -68,7 +62,6 @@ pub fn default(points: [Pos2; 2], color: Color32, width: f32) -> Vec<Shape> {
 	}]
 }
 
-
 pub fn sidewalks(way: &Way, points: [Pos2; 2], color: Color32, width: f32) -> Vec<Shape> {
 	let mut shapes = Vec::with_capacity(3);
 
@@ -77,28 +70,50 @@ pub fn sidewalks(way: &Way, points: [Pos2; 2], color: Color32, width: f32) -> Ve
 		stroke: PathStroke::new(width, color),
 	});
 
-	if let Some(tags) = &way.tags {
-		if let Some(highway) = tags.get("highway") {
-			if !HIGHWAYS_WITH_SIDEWALK.contains(&highway.as_str()) { return shapes; };
 
-			let start = points[0];
-			let end = points[1];
-			
-			let orthogonal = (end - start).normalized().rot90();
-			let offset = orthogonal * width;
+	if way.tags.keys().any(|k| k.starts_with("sidewalk")) {
+		if !sidewalks_relevant(&way.tags) { return shapes; };
+		let attr = Attribute2D::new(&way.tags, "sidewalk");
 
-			let attr = Attribute2D::new(tags, "sidewalk");
+		let from = points[0];
+		let to = points[1];
 
-			shapes.push(Shape::LineSegment {
-				points: [start + offset, end + offset],
-				stroke: PathStroke::new(width, attr.left),
-			});
-			shapes.push(Shape::LineSegment {
-				points: [start - offset, end - offset],
-				stroke: PathStroke::new(width, attr.right),
-			});
-		}
-	};
+		let orthogonal = (to - from).normalized().rot90();
+		let offset = orthogonal * width;
+
+		shapes.push(Shape::LineSegment {
+			points: [from + offset, to + offset],
+			stroke: PathStroke::new(width, attr.left),
+		});
+		shapes.push(Shape::LineSegment {
+			points: [from - offset, to - offset],
+			stroke: PathStroke::new(width, attr.right),
+		});
+
+		return shapes;
+	}
+
 
 	shapes
+}
+
+pub fn sidewalks_relevant(tags: &osm_parser::Tags) -> bool {
+	if let Some(highway) = tags.get("highway") {
+		HIGHWAYS_WITH_SIDEWALK.contains(&highway.as_str())
+	} else { false }
+}
+
+
+pub fn sidewalks_ui(ui: &mut egui::Ui, pos: Pos2) -> bool {
+	let mut open = true;
+
+	Window::new("Sidewalks")
+		.default_pos(pos)
+		.open(&mut open)
+		.show(ui.ctx(), |ui| {
+			// TODO: UI
+			ui.label("TODO");
+		});
+
+	open
 }
