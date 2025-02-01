82db2837883eb310adbc16b1f2381fa076b4cc47
swarkin <102416174+Swarkin@users.noreply.github.com>
2025-02-01 23:55:38 +0100

    Add icons

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod app;
 
 use app::MyApp;
-use eframe::egui::ViewportBuilder;
+use eframe::{egui::ViewportBuilder, icon_data::from_png_bytes};
+use std::sync::Arc;
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions {
-        viewport: ViewportBuilder::default().with_inner_size([1000.0, 800.0]),
-        ..Default::default()
-    };
-    
-    eframe::run_native(
-        "walkers-editor",
-        options,
-        Box::new(|cc| Ok(Box::new(MyApp::new(cc.egui_ctx.clone())))),
-    )
+	let options = eframe::NativeOptions {
+		viewport: ViewportBuilder::default()
+			.with_inner_size([1000.0, 800.0])
+			.with_icon(Arc::from(from_png_bytes(&include_bytes!("../assets/walkers64.png")[..])
+				.expect("failed to load icon"))),
+		..Default::default()
+	};
+
+	eframe::run_native(
+		"walkers-editor",
+		options,
+		Box::new(|cc| Ok(Box::new(MyApp::new(cc.egui_ctx.clone())))),
+	)
 }
