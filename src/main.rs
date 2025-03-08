4a4486753c24b3bd2abed2f0f4338b12d6292958
Swarkin <102416174+Swarkin@users.noreply.github.com>
2025-03-08 17:37:25 +0100

    Fix window going out of bounds issue

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() -> Result<(), eframe::Error> {
 	let options = eframe::NativeOptions {
 		viewport: ViewportBuilder::default()
 			.with_inner_size([980.0, 720.0])
+			.with_clamp_size_to_monitor_size(true)
 			.with_icon(Arc::from(from_png_bytes(&include_bytes!("../assets/walkers64.png")[..])
 				.expect("failed to load icon"))),
 		..Default::default()
