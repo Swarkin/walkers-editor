20a0bb90eff9ac4c69981311f00920e6fa18b2d6
Swarkin <102416174+Swarkin@users.noreply.github.com>
2025-08-01 18:47:49 +0200

    Resolve warnings

--- src/app/editor/consts/shortcuts.rs
+++ src/app/editor/consts/shortcuts.rs
@@ -2,6 +2,7 @@ use eframe::egui::{Key, KeyboardShortcut, Modifiers};
 
 const CTRL_SHIFT: Modifiers = Modifiers { ctrl: true, shift: true, alt: false, command: false, mac_cmd: false };
 
+#[cfg(not(feature = "kiosk"))]
 pub static FULLSCREEN: &KeyboardShortcut = &KeyboardShortcut::new(Modifiers::NONE, Key::F11);
 
 pub static DOWNLOAD: &KeyboardShortcut = &KeyboardShortcut::new(CTRL_SHIFT, Key::ArrowDown);
